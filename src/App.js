2f34a34033b2a0666ba57a901087b32551b249d0
amey09 <ameymhatre09@gmail.com>
2023-11-10 23:52:34 +0530

    refactor: remove loginChecker
    This commit removes the isLoggedIn and setIsLoggedIn as it un-necessary.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import Navbar from "./components/Navbar";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import MainAppScreen from "./screens/MainAppScreen";
 import DietPlanScreen from "./screens/DietPlanScreen";
-
+import { useState } from "react";
 
 function App() {
-
-    return (
-        <>
-            <Navbar/>
-            <Routes>
-                <Route path={'/'} element={<MainAppScreen/>}/>
-                <Route path={'/diet-plan'} element={<DietPlanScreen/>}/>
-            </Routes>
-        </>
-    )
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  return (
+    <>
+      <Navbar isLoggedIn={isLoggedIn} />
+      <Routes>
+        <Route path={"/"} element={<MainAppScreen />} />
+        <Route path={"/diet-plan"} element={<DietPlanScreen />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
