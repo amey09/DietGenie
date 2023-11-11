740f6a3ac002dcb46171566287a7afd10b688ce7
amey09 <ameymhatre09@gmail.com>
2023-11-11 23:21:50 +0530

    feat: add dietplan search functionality
    This commit allows users to search for dietplan utilizing the getDietPlan api endpoint. Whislt drastically improving the user experince thorught the search process by adding required validations utilizing isDisabled props.

--- src/utils/dietSchema.js
+++ src/utils/dietSchema.js
@@ -1,64 +1,7 @@
 export const dietPlanSchema = {
-    "Today's Plan": {
-        "Breakfast": [
-            {
-                "Food": "Eggs",
-                "Protein": 6.5,
-                "Carbs": 0.4,
-                "Fats": 5
-            },
-            {
-                "Food": "Turkey Bacon",
-                "Protein": 5,
-                "Carbs": 0.3,
-                "Fats": 3
-            },
-            {
-                "Food": "Oatmeal",
-                "Protein": 5,
-                "Carbs": 27,
-                "Fats": 3
-            }
-        ],
-        "Lunch": [
-            {
-                "Food": "Chicken Sandwich",
-                "Protein": 23,
-                "Carbs": 46,
-                "Fats": 10
-            },
-            {
-                "Food": "Spinach Salad",
-                "Protein": 2.4,
-                "Carbs": 5.3,
-                "Fats": 0.2
-            },
-            {
-                "Food": "Strawberries",
-                "Protein": 0.5,
-                "Carbs": 11.7,
-                "Fats": 0.3
-            }
-        ],
-        "Dinner": [
-            {
-                "Food": "Salmon",
-                "Protein": 22,
-                "Carbs": 0,
-                "Fats": 13
-            },
-            {
-                "Food": "Quinoa",
-                "Protein": 8,
-                "Carbs": 21.3,
-                "Fats": 2
-            },
-            {
-                "Food": "Green Beans",
-                "Protein": 3.4,
-                "Carbs": 7.3,
-                "Fats": 0.2
-            }
-        ]
-    }
+  "Today's Diet": {
+    BREAKFAST: ["FoodItem1", "FoodItem2", "FoodItem3"],
+    LUNCH: ["FoodItem1", "FoodItem2", "FoodItem3"],
+    DINNER: ["FoodItem1", "FoodItem2", "FoodItem3"],
+  },
 };
